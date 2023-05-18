0e6ad3c5984cd79bbe33a2c72d679aca28032084
FredPeal <frederickpeal@gmail.com>
2023-05-17 23:30:59 -0400

    refactor: remove comment

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,7 +4,6 @@ export const Image = (props) => {
     return (
         <>
             <div className="preview-image d-flex align-items-center justify-content-center">
-                {/* Aquí puedes colocar tu lógica para mostrar la imagen */}
                 <img src={props.src} alt="Previsualización" className="img-fluid" />
             </div>       
         </>
